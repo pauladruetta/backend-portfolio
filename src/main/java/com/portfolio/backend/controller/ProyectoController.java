ca00e01a2cecf491eb92033eee281e419c989994
Paula Druetta <druetta.paula@gmail.com>
2022-08-20 10:09:26 -0300

    FIX Cors

--- src/main/java/com/portfolio/backend/controller/ProyectoController.java
+++ src/main/java/com/portfolio/backend/controller/ProyectoController.java
@@ -18,8 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyecto")
-//@CrossOrigin(origins = "http://localhost:4200", allowedHeaders="*")
-@CrossOrigin(origins = "https://frontendap-c120f.web.app")
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders="*")
+//@CrossOrigin(origins = "https://frontendap-c120f.web.app")
 public class ProyectoController {
         
     private final IProyectoService proyectoServ;
