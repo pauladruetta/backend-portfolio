1b7bf8f436c36f2fe6c3abb59770f9e0a1661033
Paula Druetta <druetta.paula@gmail.com>
2022-08-22 23:41:41 -0300

    Merge branch 'desarrollo' of https://github.com/pauladruetta/backend-portfolio into desarrollo
    # Conflicts:
    #	src/main/resources/application.properties

--- src/main/java/com/portfolio/backend/controller/PersonaController.java
+++ src/main/java/com/portfolio/backend/controller/PersonaController.java
@@ -21,8 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/persona")
 //@CrossOrigin(origins = "http://localhost:4200", allowedHeaders="*")
 @CrossOrigin(origins = "https://frontendap-c120f.web.app")
-//@CrossOrigin(origins = "**")
-//@CrossOrigin
+
 public class PersonaController {
 
     
